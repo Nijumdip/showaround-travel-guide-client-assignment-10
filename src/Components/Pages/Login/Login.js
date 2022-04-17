9ea15e46999c9c30f950b58eb0d959ff0a2cd240
Nijumdip <nijumdip019911@gmail.com>
2022-04-17 21:54:39 +0600

    added login form

--- src/Components/Pages/Login/Login.js
+++ src/Components/Pages/Login/Login.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-        Loginnnnnn
-        </div>
+        <div className="container w-50 mx-auto m-5">
+      <h2  className="text-primary text-center ">Please Login</h2>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Control type="email" placeholder="Enter email" required/>
+          
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Control type="password" placeholder="Password" required/>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button  variant="primary w-50 d-block mx-auto mb-3" type="submit">
+            Login
+        </Button>
+      </Form>
+      <p>New To Showaround Travel Guide ? <Link to='/register' className="text-primary text-decoration-none">Please Register</Link></p>
+      <p>Forget Password ? <button  className="btn btn-link text-primary text-decoration-none">Reset Password</button></p>
+    </div>
     );
 };
 
